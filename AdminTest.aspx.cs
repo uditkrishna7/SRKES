54888960a1199d8c6079a32603f0ba6eaaa00060
uditkrishna7 <138179050+uditkrishna7@users.noreply.github.com>
2024-11-08 17:35:48 +0530

    Add files via upload
    trying to host it on github pages

---
+++ AdminTest.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI.WebControls;
+
+public partial class AdminPanel : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            LoadData();
+        }
+    }
+
+    private void LoadData()
+    {
+        // Connection string
+        string connectionString = "Data Source=UDIT\\SQLEXPRESS;Initial Catalog=OrderDB;Integrated Security=True;Encrypt=False";
+
+        // SQL query to fetch data
+        string query = "SELECT * FROM Orders";
+
+        // DataTable to hold the data
+        DataTable dataTable = new DataTable();
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            try
+            {
+                // Open connection
+                connection.Open();
+
+                // Execute query using SqlCommand and SqlDataAdapter
+                using (SqlCommand command = new SqlCommand(query, connection))
+                using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                {
+                    adapter.Fill(dataTable);
+                }
+
+                // Bind data to GridView
+                AdminGridView.DataSource = dataTable;
+                AdminGridView.DataBind();
+            }
+            catch (Exception ex)
+            {
+                // Display error message
+                Response.Write("An error occurred: " + ex.Message);
+            }
+        }
+    }
+}
